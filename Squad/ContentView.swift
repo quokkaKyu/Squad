ee70d4d498a96de6e99181f070499e6dfdd68151
quokkaKyu <kyumin.dev@gmail.com>
2024-06-25 15:52:53 +0900

    Initial Commit

---
+++ Squad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Squad
+//
+//  Created by kyuminlee on 6/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Squad/SquadApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquadApp.swift
+//  Squad
+//
+//  Created by kyuminlee on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SquadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
