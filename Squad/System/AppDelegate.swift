dd8cb0e35d0e4e5f47efc8f204132c3c45788519
quokkaKyu <kyumin.dev@gmail.com>
2024-06-25 16:32:31 +0900

    add: AppDelegate, SceneDelegate

---
+++ Squad/System/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Squad
+//
+//  Created by kyuminlee on 6/25/24.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+        launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
